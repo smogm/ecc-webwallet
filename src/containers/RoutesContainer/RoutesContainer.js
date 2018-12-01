c3b5c57ac5d9c86715c14388ac1cea9870baa44d
kdbusiness121 <kdbusiness1994@gmail.com>
2018-12-02 02:42:19 +0800

    completed issue7 keystore upload page

--- src/containers/RoutesContainer/RoutesContainer.js
+++ src/containers/RoutesContainer/RoutesContainer.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 import loginContainer from 'containers/loginContainer/loginContainer';
 import walletConfirmContainer from 'containers/walletConfirmContainer/walletConfirmContainer';
+import keystoreUploadContainer from 'containers/keystoreUploadContainer/keystoreUploadContainer';
 
 class RoutesContainer extends PureComponent {
 
@@ -10,7 +11,8 @@ class RoutesContainer extends PureComponent {
     return (
       <Switch>
         <Route exact path="/login" component={loginContainer}/>
-        <Route exact path="/confirm" component={walletConfirmContainer}/>
+        <Route exact path="/create" component={walletConfirmContainer}/>
+        <Route exact path="/unlock" component={keystoreUploadContainer}/>
         <Route path="/" component={loginContainer}/>
         <Redirect to="/404"/>
       </Switch>
